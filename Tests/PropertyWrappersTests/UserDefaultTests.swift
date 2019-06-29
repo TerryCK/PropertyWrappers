ae58265134c969e8837e4d898f28bcf93fe567c8
Ole Begemann <ole@oleb.net>
2019-06-29 22:53:20 +0200

    Add more UserDefault tests

--- Tests/PropertyWrappersTests/UserDefaultTests.swift
+++ Tests/PropertyWrappersTests/UserDefaultTests.swift
@@ -2,12 +2,14 @@ import PropertyWrappers
 import XCTest
 
 final class PropertyWrappersTests: XCTestCase {
-  var defaults: UserDefaults!
+  private var defaults: UserDefaults!
+  private var sut: UserDefaultContainer!
 
   override func setUp() {
     super.setUp()
     defaults = UserDefaults(suiteName: "unit-tests")!
     defaults.removePersistentDomain(forName: "unit-tests")
+    sut = UserDefaultContainer(defaults: defaults)
   }
 
   override func tearDown() {
@@ -16,27 +18,43 @@ final class PropertyWrappersTests: XCTestCase {
   }
 
   func testAssignmentSetsUserDefault() {
-    let sut = UserDefaultContainer(defaults: defaults)
     XCTAssertNil(defaults.string(forKey: "test-localeIdentifier"))
     sut.localeIdentifier = "de_DE"
     XCTAssertEqual(defaults.string(forKey: "test-localeIdentifier"), "de_DE")
   }
 
   func testGetterRetrievesUserDefault() {
-    let sut = UserDefaultContainer(defaults: defaults)
     sut.firstLaunch = Date(timeIntervalSinceReferenceDate: 400_000_000)
     XCTAssertEqual(sut.firstLaunch.timeIntervalSinceReferenceDate, 400_000_000)
   }
 
   func testUsesDefaultValueIfNotSet() {
-    let sut = UserDefaultContainer(defaults: defaults)
     XCTAssertEqual(sut.favoriteNumber, 42)
   }
 
   func testSetterIsNonmutating() {
-    let sut = UserDefaultContainer(defaults: defaults)
     sut.favoriteNumber = 23
     // No assertion needed. The test is that assigning to a let variable compiles.
+  }
+
+  func testArray() {
+    sut.cities = ["Berlin"]
+    sut.cities.append("London")
+    XCTAssertEqual(defaults.array(forKey: "test-cities") as? [String], ["Berlin", "London"])
+    XCTAssertEqual(sut.cities, ["Berlin", "London"])
+  }
+
+  func testDictionary() {
+    sut.menu = ["File": ["New", "Open", "Save"], "Edit": ["Cut", "Copy", "Paste"]]
+    XCTAssertEqual(defaults.dictionary(forKey: "test-menu") as? [String: [String]], ["File": ["New", "Open", "Save"], "Edit": ["Cut", "Copy", "Paste"]])
+    XCTAssertEqual(sut.menu, ["File": ["New", "Open", "Save"], "Edit": ["Cut", "Copy", "Paste"]])
+  }
+
+  func testCustomType() {
+    XCTAssertEqual(sut.selectedTab, .timeline)
+    sut.selectedTab = .profile
+    XCTAssertEqual(defaults.string(forKey: "test-selectedTab"), "profile")
+    XCTAssertEqual(sut.selectedTab, .profile)
   }
 }
 
@@ -47,11 +65,31 @@ fileprivate struct UserDefaultContainer {
   @UserDefault var favoriteNumber: Int
   @UserDefault var localeIdentifier: String
   @UserDefault var firstLaunch: Date
+  @UserDefault var cities: [String]
+  @UserDefault var menu: [String: [String]]
+  @UserDefault var selectedTab: Tab
 
   init(defaults: UserDefaults) {
     self.defaults = defaults
     $favoriteNumber = UserDefault(key: "test-favoriteNumber", defaultValue: 42, userDefaults: defaults)
     $localeIdentifier = UserDefault(key: "test-localeIdentifier", defaultValue: "en_US", userDefaults: defaults)
     $firstLaunch = UserDefault(key: "test-firstLaunch", defaultValue: Date(timeIntervalSinceReferenceDate: 500_000_000), userDefaults: defaults)
+    $cities = UserDefault(key: "test-cities", defaultValue: [], userDefaults: defaults)
+    $menu = UserDefault(key: "test-menu", defaultValue: [:], userDefaults: defaults)
+    $selectedTab = UserDefault(key: "test-selectedTab", defaultValue: .timeline, userDefaults: defaults)
+  }
+}
+
+fileprivate enum Tab: String, PropertyListConvertible {
+  case timeline
+  case profile
+  case search
+
+  init?(propertyListValue: String) {
+    self.init(rawValue: propertyListValue)
+  }
+
+  var propertyListValue: String {
+    return rawValue
   }
 }
